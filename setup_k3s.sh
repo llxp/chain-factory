629295857c3e0a80b63e58f1ca1fbc92e8cf4a56
Lukas Lüdke <lukasluedke@web.de>
2022-06-27 14:45:01 +0200

    ...

--- setup_k3s.sh
+++ setup_k3s.sh
@@ -2,7 +2,6 @@
 # echo $k3s_token > ./k3s/k3s_token
 # curl -sfL https://get.k3s.io | K3S_TOKEN=$k3s_token sh -s - server --cluster-init
 sudo -i -u vagrant curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -
-ln -s /etc/rancher/k3s/k3s.yaml ~/.kube/config
 
 apt install -y nfs-kernel-server nfs-common
 mkdir -p /exports/k3s
@@ -34,3 +33,4 @@ EOF
 curl -fsSL -o /tmp/get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 /tmp/get_helm.sh
 /tmp/get_helm.sh
+ln -s /etc/rancher/k3s/k3s.yaml ~/.kube/config
