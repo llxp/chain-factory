51c5f067512d5bf2e79a9df688efb47fed0917bb
Lukas Lüdke <lukasluedke@web.de>
2022-06-28 20:40:24 +0200

    ...

--- scripts/provision_mongodb.sh
+++ scripts/provision_mongodb.sh
@@ -8,6 +8,7 @@ export PATH=$PATH:/usr/local/go/bin
 git clone https://github.com/mongodb/mongo-tools
 cd mongo-tools
 ./make build
+cd ..
 
 kubectl port-forward svc/example-mongodb-svc 27017:27017 --namespace=mongodb-operator &
 ./bin/mongorestore -u my-admin -p Start123 -h 127.0.0.1:27017 --db db --authenticationDatabase admin ./mongodb/db/
