bd407f30ed6aed1efd3c1f03c27ae21c6f01c83e
Lukas Lüdke <lukasluedke@web.de>
2022-06-28 20:12:15 +0200

    fixing provision_mongodb.sh

--- scripts/provision_mongodb.sh
+++ scripts/provision_mongodb.sh
@@ -1,11 +1,13 @@
 #! /bin/sh
 
+apt install -y build-essential libkrb5-dev
 wget https://go.dev/dl/go1.18.3.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.18.3.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 git clone https://github.com/mongodb/mongo-tools
-./mongo-tools/make build
+cd mongo-tools
+./make build
 
-kubectl port-forward svc/example-mongodb-svc 27017:27017 &
-mongorestore -u my-admin -p Start123 -h 127.0.0.1:27017 --db db --authenticationDatabase admin ./mongodb/db/
+kubectl port-forward svc/example-mongodb-svc 27017:27017 --namespace=mongodb-operator &
+./bin/mongorestore -u my-admin -p Start123 -h 127.0.0.1:27017 --db db --authenticationDatabase admin ./mongodb/db/
